5f4a34240a61568b36c34d1a01592e899c296099
Graham Weldon <graham@grahamweldon.com>
2012-08-13 09:32:16 +1000

    Changes to use Collidable, fallable arrays for optimisation. Added a testing faller to check multiple objects.

--- SpaceEmu.js
+++ SpaceEmu.js
@@ -3,16 +3,19 @@
  * ------------------------------
  */
 var Player = function(context, x, y) {
+	this.gravity = true;
+	this.collidable = true;
 	this.context = context;
 	this.x = x;
 	this.y = y;
-	this.radius = 20;
+	this.width = 15;
+	this.height = 15;
 	this.fillStyle = '#fff';
 }
 Player.prototype.draw = function() {
 	this.context.fillStyle = this.fillStyle;
 	this.context.beginPath();
-	this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+	this.context.arc(this.x, this.y, this.height, 0, Math.PI * 2, true);
 	this.context.closePath();
 	this.context.fill();
 };
@@ -22,6 +25,8 @@ Player.prototype.draw = function() {
  * ------------------------------
  */
 var Platform = function(context, x, y, width, height) {
+	this.gravity = false;
+	this.collidable = true;
 	this.context = context;
 	this.x = x;
 	this.y = y;
@@ -44,6 +49,10 @@ var SpaceEmu = function() {
 
 	this.player = null;
 	this.platforms = [];
+	
+	this.fallable = [];
+	this.collidable = [];
+	this.objects = [];
 };
 
 SpaceEmu.prototype.initialize = function() {
@@ -60,12 +69,33 @@ SpaceEmu.prototype.initialize = function() {
 
 SpaceEmu.prototype.gameSetup = function() {
 	this.player = new Player(this.context, 100, 50);
+	this.addObject(this.player);
 
-	this.platforms.push(new Platform(this.context, 50, 130, 200, 20));
-	this.platforms.push(new Platform(this.context, 380, 130, 200, 20));
-	this.platforms.push(new Platform(this.context, 50, 280, 200, 20));
-	this.platforms.push(new Platform(this.context, 380, 280, 200, 20));
-	this.platforms.push(new Platform(this.context, 0, 420, 640, 20));
+	var p2 = new Player(this.context, 320, 50);
+	this.addObject(p2);
+
+
+	this.createPlatform(50, 130, 200, 20);
+	this.createPlatform(380, 130, 200, 20);
+	this.createPlatform(50, 280, 200, 20);
+	this.createPlatform(380, 280, 200, 20);
+	this.createPlatform(0, 420, 640, 20);
+};
+
+SpaceEmu.prototype.createPlatform = function(x, y, width, height) {
+	var p = new Platform(this.context, x, y, width, height);
+	this.platforms.push(p);
+	this.addObject(p);
+};
+
+SpaceEmu.prototype.addObject = function(o) {
+	this.objects.push(o);
+	if (o.gravity) {
+		this.fallable.push(o);
+	}
+	if (o.collidable) {
+		this.collidable.push(o);
+	}
 };
 
 SpaceEmu.prototype.drawLoop = function() {
@@ -82,27 +112,27 @@ SpaceEmu.prototype.draw = function() {
 	this.context.fillStyle = '#111';
 	this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-	for (var i = 0; i < this.platforms.length; i++) {
-		this.platforms[i].draw();
+	for (var i in this.objects) {
+		this.objects[i].draw();
 	}
-
-	this.player.draw();
 };
 
 SpaceEmu.prototype.update = function() {
-	this.player.x += 1;
-	this.player.y += 4;
+	for (var i in this.fallable) {
+		this.fallable[i].x += 1;
+		this.fallable[i].y += 4;
 
-	for (var i = 0; i < this.platforms.length; i++) {
-		var platform = this.platforms[i];
+		for (var j in this.collidable) {
+			var collider = this.collidable[j];
 
-		// If collision of bottom edge of the player with top edge of a platform
-		if (this.player.y + this.player.radius > platform.y &&
-			this.player.y + this.player.radius < platform.y + platform.height &&
-			this.player.x + this.player.radius > platform.x &&
-			this.player.x - this.player.radius < platform.x + platform.width) {
+			// If collision of bottom edge of the player with top edge of a platform
+			if (this.fallable[i].y + this.fallable[i].height > collider.y &&
+				this.fallable[i].y + this.fallable[i].height < collider.y + collider.height &&
+				this.fallable[i].x + this.fallable[i].width > collider.x &&
+				this.fallable[i].x - this.fallable[i].width < collider.x + collider.width) {
 
-			this.player.y = platform.y - this.player.radius;
+				this.fallable[i].y = collider.y - this.fallable[i].height;
+			}
 		}
 	}
 };
